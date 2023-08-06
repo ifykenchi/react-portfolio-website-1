b5305511f204a365e0bfede66f425f81fe280fb1
ifykenchi <ifykenchi@gmail.com>
2023-08-06 12:48:38 +0100

    working on the header section

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://linkedin.com" target='blank'></a>
+        <a href="https://github.com" target='blank'></a>
+        <a href="https://dribble.com" target='blank'></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
