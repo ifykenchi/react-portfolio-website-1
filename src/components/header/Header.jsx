5a405e39b6915bcba7aeac2d68422cf5c118e638
ifykenchi <ifykenchi@gmail.com>
2023-08-06 21:06:53 +0100

    nav section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Ifeanyichukwu Chimbo</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
+        <HeaderSocial />
 
         <div className="me">
           <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target='blank'></a>
-        <a href="https://github.com" target='blank'></a>
-        <a href="https://dribble.com" target='blank'></a>
+        <a href="https://linkedin.com" target='blank'><BsLinkedin/></a>
+        <a href="https://github.com" target='blank'><FaGithub/></a>
+        <a href="https://dribble.com" target='blank'><FiDribbble/></a>
     </div>
   )
 }
