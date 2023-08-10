d7bd6d78726aa64f455ef1b613bc143b3e61c878
ifykenchi <ifykenchi@gmail.com>
2023-08-10 08:37:47 +0100

    worked on the nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,15 +4,18 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience"><BiBook /></a>
       <a href="#services"><RiServiceLine /></a>
-      <a href="#contact"><AiOutlineHome /></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
     </nav>
   )
 }
